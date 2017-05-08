b9d82456f70cd25dc8a0cecfdab9a2ed03af4f3a
Ricky Li <RLi@nowcom.com>
2017-05-08 15:37:56 -0700

    Generate blank gif

--- SampleWebApp/Controllers/HomeController.cs
+++ SampleWebApp/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ImageSharp;
 
+
 namespace SampleWebApp.Controllers
 {
     public class HomeController : Controller
@@ -35,8 +36,15 @@ namespace SampleWebApp.Controllers
 
         public IActionResult Test()
         {
-            Configuration.Default.AddImageFormat(new ImageSharp.Formats.JpegFormat());
-            return Content("Hello");
+            string onePixelGifBase64 = "R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+            byte[] content = Convert.FromBase64String(onePixelGifBase64);
+            Configuration.Default.AddImageFormat(new ImageSharp.Formats.GifFormat());
+            var image = Image.Load(content);
+            var ms = new System.IO.MemoryStream();
+            image.Resize(new ImageSharp.Processing.ResizeOptions { Size = new Size(100, 100) });
+            image.Save(ms);
+            var newContent = ms.ToArray();
+            return File(newContent, "image/gif");
         }
     }
 }
